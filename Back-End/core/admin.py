7e839dc83036d2fab487bedd57a0bb26b13dba04
Saeed Eslampour <eslampoursaeed278@gmail.com>
2023-07-27 13:59:37 +0330

    Create custom user model and bring up its admin page.

--- Back-End/core/admin.py
+++ Back-End/core/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+
+# admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    readonly_fields = ["date_joined"]
+  
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ("first_name", 
+                                        "last_name", 
+                                        "email", 
+                                        "image", 
+                                        "gender", 
+                                        "language"
+                                        )}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
--- Back-End/core/models.py
+++ Back-End/core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from persiantools.jdatetime import JalaliDate
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Add extra fields to the default built-in django AbstractUser class
+    and also has the default django permission for the User model
+
+    username, password and email fields are required in the current implementation.
+    """
+
+    GENDER_CHOICES = [
+        ('مرد', 'مرد'),
+        ('زن', 'زن'),
+    ]
+
+    LANGUAGE_CHOICES = [
+        ('فارسی', 'فارسی'),
+        ('انگلیسی', 'انگلیسی'),
+    ]
+
+    first_name = models.CharField(max_length=25, verbose_name='نام', blank=True)
+    last_name = models.CharField(max_length=30, verbose_name='نام خانوادگی', blank=True)
+    email = models.EmailField(unique=True, verbose_name='آدرس ایمیل')
+    image = models.ImageField(upload_to='core/images', verbose_name='تصویر', null=True, blank=True)
+    gender = models.CharField(max_length=3, choices=GENDER_CHOICES, verbose_name='جنسیت', null=True, blank=True)
+    language = models.CharField(max_length=10, choices=LANGUAGE_CHOICES, verbose_name='زبان', null=True, blank=True)
+    date_joined = models.DateTimeField('تاریخ عضویت', auto_now_add=True)
+    is_active = models.BooleanField('فعال', default=True)
+    is_staff = models.BooleanField('پرسنل', default=False)
+    is_superuser = models.BooleanField('ادمین', default=False)
+
+
+    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
+
+    class Meta:
+        verbose_name = ("کاربر")
+        verbose_name_plural = ("کاربرها")
+
+    def get_joined_persian_time(self):
+        return JalaliDate(self.date_joined, locale=('fa')).strftime('%c')
+
+    get_joined_persian_time.title = 'تاریخ ثبت نام'    
+
+    def __str__(self):
+        return self.username
