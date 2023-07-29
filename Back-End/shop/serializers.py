4e397ebdcf7a88204d4468c84096cfaad483ac88
Saeed Eslampour <eslampoursaeed278@gmail.com>
2023-07-29 22:36:00 +0330

    Add the endpoint to get all products

---
+++ Back-End/shop/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Product
+
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'product_image', 
+            'title', 
+            'slug', 
+            'price', 
+            'description', 
+            'inventory', 
+            'last_update', 
+            'category'
+        ]
--- Back-End/shop/views.py
+++ Back-End/shop/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+# custom User model
+User = get_user_model()
+
+
+class ProductList(APIView):
+    def get(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
