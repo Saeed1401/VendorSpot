8e7dd09b74efdcc952820fd4b9a7750ee67ced4f
Saeed Eslampour <eslampoursaeed278@gmail.com>
2023-07-30 17:36:52 +0330

    revert the validation logic to the first state

--- Back-End/shop/validators.py
+++ Back-End/shop/validators.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 def file_size_validation(file):
     max_size = 600
 
-    if file.size > max_size % 1024:
+    if file.size > max_size * 1024:
         raise ValidationError(f'Files cannot be larger than {max_size}KB!!')
     
 
