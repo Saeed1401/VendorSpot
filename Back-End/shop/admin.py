312b8c5bec4723e7f6cfb28e24a1e693404adfce
Saeed Eslampour <eslampoursaeed278@gmail.com>
2023-07-31 14:19:13 +0330

    setting up the admin interface for customer, product and category models

--- Back-End/shop/admin.py
+++ Back-End/shop/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+    'title', 
+    'slug', 
+    'price', 
+    'product_image', 
+    'description', 
+    'inventory', 
+    'last_update', 
+    'category'
+]
+    list_per_page = 10
+
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'thumbnail']
+    list_display_links = ['thumbnail']
+    list_editable = ['title']
+    list_per_page = 10
+
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'birth_date']
+    list_per_page = 10
