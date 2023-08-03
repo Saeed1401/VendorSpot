9f6ab0d408c57f70768e667852fe1f509a202dfe
Saeed Eslampour <eslampoursaeed278@gmail.com>
2023-08-04 02:27:54 +0330

    add a New field for confirming the password in create user endpoint

--- Back-End/core/serializers.py
+++ Back-End/core/serializers.py
@@ -1,14 +1,36 @@
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer, UserSerializer as BaseUserSerializer
-
+from djoser.serializers import (
+    UserCreateSerializer as BaseUserCreateSerializer, 
+    UserSerializer as BaseUserSerializer,
+)
+from rest_framework import serializers
 
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
+    confirm_password = serializers.CharField(
+        write_only=True, 
+        style={
+            "input_type": "password",
+            "placeholder": "confirm your password",
+        }
+    )
 
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError('Passwords do not match!!')
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2', None)
+        return super().create(validated_data)
+    
+ 
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ['id', 
+        fields = [
+            'id', 
             'username', 
             'password', 
+            'confirm_password',
             'email', 
             'first_name', 
             'last_name', 
@@ -16,11 +38,13 @@ class UserCreateSerializer(BaseUserCreateSerializer):
             'gender'
         ]
 
+
         
 
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
-        fields = ['id', 
+        fields = [
+            'id', 
             'username', 
             'password', 
             'email', 
