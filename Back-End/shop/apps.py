33673d46021a9d5e91bc3b01481d609eaf9874c7
Saeed Eslampour <eslampoursaeed278@gmail.com>
2023-08-04 20:55:41 +0330

    implement signals to create customer record for every new User

--- Back-End/shop/apps.py
+++ Back-End/shop/apps.py
@@ -5,3 +5,6 @@ class ShopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shop'
     verbose_name = 'اپ خرید'
+
+    def ready(self) -> None:
+        import shop.signals.handlers
