cfd7c11b6a71af42f8e7c1717635034b37b78dac
Saeed Eslampour <eslampoursaeed278@gmail.com>
2023-08-10 11:28:33 +0330

    customize the activation url provided by djoser

--- Back-End/core/urls.py
+++ Back-End/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'core'
+
 urlpatterns = [
-    
+    path('auth/users/activate/<str:uidb64>/<str:token>/', views.ActivateUserView.as_view(), name='custom_activation'),
 ]
--- Back-End/core/views.py
+++ Back-End/core/views.py
@@ -1,2 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from django.utils.http import urlsafe_base64_decode
 
+
+User = get_user_model()
+
+
+class ActivateUserView(APIView):
+    """
+    check to see if the uid and token is valid or not, if it is then,
+    it activates the user
+    """
+
+    def get(self, request, uidb64, token):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user is not None and default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return Response({"message": "User activated successfully."}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Invalid activation link."}, status=status.HTTP_400_BAD_REQUEST)
